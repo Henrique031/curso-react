6af15cdf3228ab56f62f1f200d200f113725159c
Henrique Galdino <102200241+Henrique031@users.noreply.github.com>
2022-08-24 03:28:01 -0300

    atualização

--- projetoApp1/App.js
+++ projetoApp1/App.js
@@ -1,16 +1,32 @@
 import React from 'react';
-import { Text, View, SafeAreaView, Button, TextInput, ImageBackground } from 'react-native';
-import ImgPets from './assets/pets'
+import { SafeAreaView, StyleSheet } from 'react-native';
+
+import MegaNumero from './src/Components/Mega/MegaNumero';
+// import Mega from './src/Components/Mega/Mega'
+// import ListaProdutos from './src/Components/Produtos/ListaProdutos';
+// import ListaProdutosV2 from './src/Components/Produtos/ListaProdutosV2';
 
 export default function Pricipal()  {
 
   return (
-    <SafeAreaView style={{flex: 1}} >
-      <View>
-      <ImageBackground source={ImgPets}  >
-        
-      </ImageBackground>
-      </View>
+    <SafeAreaView style={styles.container} >
+      <MegaNumero />
+    {/* 
+    <Mega qtdeNumeros={10} />
+    <ListaProdutosV2 />
+    <ListaProdutos /> 
+    */}
+
     </SafeAreaView>
   )
-}
+};
+
+const styles = StyleSheet.create({
+  container: {
+    marginTop: 23,
+    justifyContent: 'center',
+    alignItems: 'center',
+    flex: 1,
+    backgroundColor: 'lightgray',
+  }
+});
--- projetoApp1/src/Components/Produtos/ListaProdutosV2.js
+++ projetoApp1/src/Components/Produtos/ListaProdutosV2.js
@@ -2,11 +2,14 @@ import React from 'react'
 import { Text, FlatList } from 'react-native'
 import Estilo from '../estilo'
 
-import produtos from './produtos'
+import produtos from './Produtos'
 
 export default props => {
-    const produtoRender = ({ item: p }) => {
-        return <Text>{p.id}) {p.nome} - R$ {p.preco}</Text>
+ 
+    const ProdutoRender = ({ item: p}) => {
+        return(
+            <Text>{p.id}) {p.nome} R$ {p.preco} </Text>
+        )
     }
 
     return (
@@ -17,7 +20,7 @@ export default props => {
             <FlatList
                 data={produtos}
                 keyExtractor={i => `${i.id}`}
-                renderItem={produtoRender}
+                renderItem={ProdutoRender} 
             />
         </>
     )
